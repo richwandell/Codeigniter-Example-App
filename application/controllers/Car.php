602caaaac527d73379accc8a8c9cabe04d7f2e6f
Rich Wandell <rich.wandell@ubm.com>
2015-07-15 21:18:23 -0400

    first

--- application/controllers/Car.php
+++ application/controllers/Car.php
@@ -1,9 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
+/**
+ * Class Car
+ *
+ * The Car controller Handles everything related to the Car page.
+ */
 class Car extends CI_Controller {
-
+  /**
+   * This carList will display a list of cars with the number of parts and
+   * passengers in this car.
+   *
+   * This page will be cached for up to 1 day.
+   */
   public function carlist()
   {
     $this->output->cache(1440);
@@ -13,7 +22,14 @@ class Car extends CI_Controller {
       "cars" => $cars
     ));
   }
-
+  /**
+   * The passengerList page will display a list of passengers that are in
+   * a car. It will use the provided car id to find the passengers for this car.
+   *
+   * This page is cached for up to 1 day and invalidated when this car is deleted
+   * or when a new passenger is added to this car.
+   * @param $car_id
+   */
   public function passengerList($car_id)
   {
     $this->output->cache(1440);
@@ -24,7 +40,12 @@ class Car extends CI_Controller {
       "car" => $car
     ));
   }
-
+  /**
+   * The partList page will display a list of parts for the provided car id.
+   * This page is cached for up to 1 day and it is invalidated when the car is
+   * deleted or when a new part is added to this car.
+   * @param $car_id
+   */
   public function partList($car_id)
   {
     $this->output->cache(1440);
@@ -36,10 +57,15 @@ class Car extends CI_Controller {
     ));
   }
 
+  /**
+   * The addCar function is used to relieve a form post request to add a new
+   * car.
+   */
   public function addCar()
   {
     try {
-      $this->load->library('user_agent');
+      //Check if we have the car_name variable. If not we add a missing variable
+      //to the $missing array.
       $missing = array();
       if (!$this->input->post('car_name') or trim($this->input->post('car_name')) === "") {
         $missing["car_name"] = "Car Name Required";
@@ -47,82 +73,126 @@ class Car extends CI_Controller {
         $this->session->set_userdata("car_name_value", $this->input->post('car_name'));
       }
 
+      //If were missing anything we throw the missing exception and provide the
+      //missing parameters array.
       if (count($missing) > 0) {
+        //MissingParametersException is a custom exception in the libraries folder
+        //It logs the message provided and has a getMissing method used in the catch clause
         throw new MissingParametersException($missing, "addPassenger");
       }
 
+      //Create a new car and set its variables.
       $car = new Entities\Car();
       $car->setName($this->input->post('car_name', TRUE));
+
+      //Save the car
       $this->doctrine->em->persist($car);
       $this->doctrine->em->flush();
+
+      //Set our success message
       $this->load->helper('url');
       $this->load->library('user_agent');
       $this->session->set_userdata("flash_message", "Success! Added a new car");
       $this->session->unset_userdata("car_name_value");
+
+      //Invalidate the car/carlist page from cache using our CacheInvalidator
       $this->load->library('CacheInvalidator');
       CacheInvalidator::delete_cache($this->agent->referrer());
       redirect($this->agent->referrer());
     }catch(MissingParametersException $e){
+
+      //Set the correct flash messages for the missing parameters and redirect
+      //to the previous page where a message will be given to the user.
       $missing = $e->getMissing();
       foreach($missing as $key => $val) {
-        $this->session->set_flashdata($key."_error", 'has-error');
-        $this->session->set_flashdata("flash_message", $val);
+        $this->session->set_userdata($key."_error", 'has-error');
+        $this->session->set_userdata("flash_message", $val);
       }
       redirect($this->agent->referrer());
     }
   }
 
+  /**
+   * The deleteCar does what it sounds like. It deletes a car.
+   */
   public function deleteCar()
   {
+    $this->load->library('user_agent');
+    $this->load->helper('url');
+    //Check if we have the car id
     if($this->input->post('car_id')){
+      //Find our car
       $car_id = $this->input->post('car_id');
       $cars_repo = $this->doctrine->em->getRepository('Entities\Car');
       $car = $cars_repo->find($car_id);
       if($car) {
+        //Remove the car
         $this->doctrine->em->remove($car);
         $this->doctrine->em->flush();
+        //Set message
         $this->session->set_userdata("flash_message", "Success! Removed a car");
+        //Invalidate cache
+        $this->load->library('CacheInvalidator');
+        CacheInvalidator::delete_cache("car/carlist");
+        CacheInvalidator::delete_cache("car/passengerList/$car_id");
+        CacheInvalidator::delete_cache($this->agent->referrer());
       }else{
         $this->session->set_userdata("flash_message", "Could not find car");
       }
-      $this->load->library('user_agent');
-      $this->load->helper('url');
-      $this->load->library('CacheInvalidator');
-      CacheInvalidator::delete_cache("car/carlist");
-      CacheInvalidator::delete_cache("car/passengerList/$car_id");
-      CacheInvalidator::delete_cache($this->agent->referrer());
+      redirect($this->agent->referrer());
+    }else{
       redirect($this->agent->referrer());
     }
   }
 
+  /**
+   * When a page is served from cache it is likely that it will have an invalid
+   * csrf token. This function is used to generate a new token as well as gather
+   * any messages or dynamic content and send it back as a JSON array loaded
+   * through AJAX.
+   */
   public function getCsrfToken()
   {
+    //Set json header
     header('Content-Type: application/json');
+    //Get all the session data
     $session_data = $this->session->all_userdata();
+    //Filter out things that we don't want to send back
     $keys = array_filter(array_keys($session_data), function($key){
       return substr($key, 0, 6) === "flash_" || substr($key, -6) === "_value";
     });
     $json_data = array_intersect_key($session_data, array_flip($keys));
+    //Unset the data that were sending back (flash messages)
     foreach($json_data as $key => $val){
       $this->session->unset_userdata($key);
     }
+    //Set up an array to be json_encoded and returned
     $return = array("dynamic_data" => array());
     foreach($json_data as $key => $val){
       $return["dynamic_data"][] = array(
+        //The selector for jQuery
         "selector" => "#$key",
+
+        //_value would be user entered data that should be saved
         "data" => substr($key, -6) === "_value" ? array(
           "attr" => "value",
           "data" => $val
+
+        //_error would be form errors
         ) : substr($key, -6) === "_error" ? array(
           "attr" => "class",
           "has-error"
+
+        //Anything else will be inserted HTML
         ) : array(
           "attr" => "html",
           "data" => $val
         )
       );
     }
+    //Add that csrf token
     $return["csrf"] = [$this->security->get_csrf_token_name(), $this->security->get_csrf_hash()];
+    //Send it back!
     echo json_encode($return);
     exit;
   }
--- readme.md
+++ readme.md
@@ -1,14 +1,27 @@
 # This project
 This project is just an example of using Codeigniter. 
 
-# Things
+# Things to pay attention to
+This application shows how page cache can be used with dynamic content. 
+Each Controller has pages that are cached for up to 1 day.
+When forms are submitted the page is then cleared from cache along with any other pages
+that would be effected by the new data. For example... if I were to add a new part to a car
+the car list page as well as the car parts list page and the parts list pages would all be 
+removed from cache because the data on that page should change.
+
+Dynamic messages and csrf tokens are loaded through ajax so that flash messages can still be used
+and forms can still be submitted see all.js file in /static/ folder for details and getCsrfToken function
+in the car controller. 
+
+
+
 ## Controllers in application/controllers
 
 * Car
     * Add function for adding a new Car
     * List function for listing the Cars
     * Delete function for deleting a Car
-    * Passenger List function for showin the Passengers in the car
+    * Passenger List function for showing the Passengers in the car
     
 ## Doctrine Entities in application/models/Entities
 * Car
